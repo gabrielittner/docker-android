29633794b97949d86d5e34fa47f8eb0446ca267f
Gabriel Ittner <gabriel.ittner@gmail.com>
2018-06-21 14:54:16 +0200

    supply jdk version as arg, improve build script

--- build.sh
+++ build.sh
@@ -1,19 +1,35 @@
 #!/bin/bash
+set -e
+
 TAG_BASE=gabrielittner/android-sdk:tools-26.1.1
 
-VERSIONS=( jdk8 jdk8-alpine jdk10 )
-VERSIONS_FULL=( jdk8u171 jdk8u171-alpine jdk10.0.1-10 )
+VERSIONS=( 8 8-alpine 10 )
+VERSIONS_FULL=( 8u171 8u171-alpine 10.0.1-10 )
+
+DEFAULT_VERSION=8
+DEFAULT_VERSION_ALPINE=8-alpine
 
 for ((i=0;i<${#VERSIONS[@]};++i)); do
-    docker build --pull --tag ${TAG_BASE}-${VERSIONS[i]} --tag ${TAG_BASE}-${VERSIONS_FULL[i]} ${VERSIONS[i]}
-    docker push ${TAG_BASE}-${VERSIONS[i]}
-    docker push ${TAG_BASE}-${VERSIONS_FULL[i]}
+    TAG_SIMPLE=${TAG_BASE}-jdk${VERSIONS[i]}
+    TAG_FULL=${TAG_BASE}-jdk${VERSIONS_FULL[i]}
+    
+    docker build \
+        --pull \
+        --tag ${TAG_SIMPLE} \
+        --tag ${TAG_FULL} \
+        --build-arg JDK_VERSION=${VERSIONS_FULL[i]} \
+        jdk${VERSIONS[i]}
+    
+    if [ "${VERSIONS[i]}" == "${DEFAULT_VERSION}" ]; then
+        docker tag ${TAG_FULL} ${TAG_BASE} 
+        docker push ${TAG_BASE} 
+    fi
+    
+    if [ "${VERSIONS[i]}" == "${DEFAULT_VERSION_ALPINE}" ]; then
+        docker tag ${TAG_FULL} ${TAG_BASE}-alpine
+        docker push ${TAG_BASE}-alpine
+    fi
+
+    docker push ${TAG_SIMPLE}
+    docker push ${TAG_FULL}
 done
-
-# default image
-docker tag ${TAG_BASE}-jdk8 ${TAG_BASE} 
-docker push gabrielittner/android-sdk:tools-26.1.1 
-
-# default alpine image
-docker tag ${TAG_BASE}-jdk8-alpine ${TAG_BASE}-alpine
-docker push gabrielittner/android-sdk:tools-26.1.1-alpine
