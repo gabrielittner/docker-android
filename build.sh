006d62b448bf5f387f3c05c4bb303a692f6ae3a4
Gabriel Ittner <gabriel.ittner@gmail.com>
2018-11-17 13:34:12 +0100

    make build.sh more dynamic to allow things like AdoptOpenJDK

--- build.sh
+++ build.sh
@@ -4,8 +4,26 @@ set -e
 SDK_TOOLS_VERSION=26.1.1
 SDK_TOOLS_REVISION=4333796
 
-VERSIONS_SHORT=( 8 8-alpine 11 12 )
-VERSIONS_FULL=( 8u181 8u181-alpine 11.0.1 12-ea-16)
+TAG_BASE=gabrielittner/android-sdk:tools-${SDK_TOOLS_VERSION}
+
+TAG_VERSION_SHORT=(
+  jdk8
+  jdk8-alpine
+  jdk11
+  jdk12
+)
+TAG_VERSION_FULL=(
+  jdk8u181
+  jdk8u181-alpine
+  jdk11.0.1
+  jdk12-ea-16
+)
+JDK_IMAGE=(
+  openjdk:8u181
+  openjdk:8u181-alpine
+  openjdk:11.0.1
+  openjdk:12-ea-16
+)
 
 SDK_TOOLS_IMAGE=android-sdk-tools-${SDK_TOOLS_VERSION}
 echo "================================================"
@@ -17,10 +35,10 @@ docker build \
     --build-arg SDK_TOOLS_REVISION=${SDK_TOOLS_REVISION} \
     sdk-tools
 
-TAG_BASE=gabrielittner/android-sdk:tools-${SDK_TOOLS_VERSION}
-for ((i=0;i<${#VERSIONS_SHORT[@]};++i)); do
-    TAG_SIMPLE=${TAG_BASE}-jdk${VERSIONS_SHORT[i]}
-    TAG_FULL=${TAG_BASE}-jdk${VERSIONS_FULL[i]}
+for ((i=0;i<${#JDK_IMAGE[@]};++i)); do
+    TAG_SIMPLE=${TAG_BASE}-${TAG_VERSION_SHORT[i]}
+    TAG_FULL=${TAG_BASE}-${TAG_VERSION_FULL[i]}
+
     echo "================================================"
     echo "Building ${TAG_FULL} image"
     echo "================================================"
@@ -29,8 +47,8 @@ for ((i=0;i<${#VERSIONS_SHORT[@]};++i)); do
         --tag ${TAG_SIMPLE} \
         --tag ${TAG_FULL} \
         --build-arg SDK_TOOLS_IMAGE=${SDK_TOOLS_IMAGE} \
-        --build-arg JDK_VERSION=${VERSIONS_FULL[i]} \
-        jdk${VERSIONS_SHORT[i]}
+        --build-arg JDK_IMAGE=${JDK_IMAGE[i]} \
+        ${TAG_VERSION_SHORT[i]}
 
     docker push ${TAG_SIMPLE}
     docker push ${TAG_FULL}
